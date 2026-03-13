1b1279f68ab3f351d3da28c49cbebb3119f4c682
anekk <aneckalarionova@gmail.com>
2026-03-13 12:52:03 +0500

    Программирование окна MainActivity. Осенников

--- app/src/main/java/com/example/pr6/MainActivity.java
+++ app/src/main/java/com/example/pr6/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.pr6;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,49 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    public  boolean active = true;
+    private SensorManager sensorManager;
+    private int count = 0;
+    private TextView text;
+    private long lastUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        text = findViewById(R.id.textView2);
+        text.setText(String.valueOf(count));
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener((SensorEventListener) this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        lastUpdate = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener((SensorEventListener) this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener((SensorEventListener) this);
+    }
+
+    public void onStoped(View view){
+        active = !active;
+        if (!active){
+            Button button = findViewById(R.id.button);
+            button.setText("Возобновить");
+        }
+        else{
+            Button button = findViewById(R.id.button);
+            button.setText("Пауза");
+        }
     }
 }
