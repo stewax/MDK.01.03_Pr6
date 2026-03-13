b5f24689102bbb750385f51d1a0a7c368a53de47
anekk <aneckalarionova@gmail.com>
2026-03-13 12:59:25 +0500

    Разработанная практическая работа на оценку 3. Осенников

--- app/src/main/java/com/example/pr6/MainActivity.java
+++ app/src/main/java/com/example/pr6/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pr6;
 
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
@@ -58,5 +59,34 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             Button button = findViewById(R.id.button);
             button.setText("Пауза");
         }
+    }
+
+    public void onSensorChanged(SensorEvent event){
+        if (event.sensor.getType()== Sensor.TYPE_ACCELEROMETER){
+            float[] values = event.values;
+            float x = values[0];
+            float y = values[1];
+            float z = values[2];
+
+            float accelationSquareRoot=(x* x+ y* y+ z* z)
+                    /(SensorManager.GRAVITY_EARTH* SensorManager.GRAVITY_EARTH);
+
+            long actualTime=System.currentTimeMillis();
+
+            if (accelationSquareRoot>=2){
+                if (actualTime- lastUpdate < 200){
+                    return;
+                }
+                lastUpdate= actualTime;
+
+                count++;
+                text.setText(String.valueOf(count));
+            }
+        }
+    }
+
+    @Override
+    public void onAccuaracyChanged(Sensor sensor, int accuracy){
+
     }
 }
